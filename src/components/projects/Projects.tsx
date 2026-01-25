9d87fb82931ad8f683d80cba2d966a3787fdac2e
Mazine Suliman <mazine_suliman@alumni.brown.edu>
2026-01-24 19:18:19 -0800

    feat- polish projects section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination, Autoplay } from "swiper";
+import SwiperCore, { Pagination, Autoplay, FreeMode } from "swiper";
 import { Slide } from "react-awesome-reveal";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/free-mode";
 
 import "./Projects.css";
 import { projectsData } from "../data/projectsData";
 
-SwiperCore.use([Pagination, Autoplay]);
+SwiperCore.use([Pagination, Autoplay, FreeMode]);
 
 const Projects: React.FC = () => {
   // Split the projectsData into two arrays for two rows
@@ -17,69 +18,106 @@ const Projects: React.FC = () => {
   const secondRowProjects = projectsData.slice(halfwayIndex);
 
   return (
-    <section className="projects container section" id="projects">
-      <Slide direction="left" triggerOnce>
-        <h2 className="section__title">Projects</h2>
-      </Slide>
-      <Slide direction="right" triggerOnce>
-        <span className="section__subtitle"></span>
-      </Slide>
+    <section className='projects section' id='projects'>
+      <div className='container'>
+        <Slide direction='left' triggerOnce>
+          <h2 className='section__title'>Projects</h2>
+        </Slide>
+        <Slide direction='right' triggerOnce>
+          <span className='section__subtitle'></span>
+        </Slide>
+      </div>
 
       {/* First Swiper instance for the first row */}
       <Swiper
-        className="projects__container"
+        className='projects__container projects__container--fullwidth'
         loop={true}
-        grabCursor={true}
-
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        allowTouchMove={false}
+        freeMode={{
+          enabled: true,
+          momentum: false,
+        }}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: false,
+        }}
+        speed={8000}
         slidesPerView={3}
         spaceBetween={24}
-        // pagination={{ clickable: true }}
-        modules={[Pagination, Autoplay]}
+        modules={[Pagination, Autoplay, FreeMode]}
       >
-        {firstRowProjects.map(({ id, title, description, image, GitHub, hosted }) => (
-          <SwiperSlide className="projects__card" key={id}>
-            <h3 className="projects__name">{title}</h3>
-            <img src={image} alt="img" className="projects__img" />
-            <div className="projects__description">{description}</div>
-            <div>
-              <a href={`${hosted}`} className="projects_social-icon" target="_blank" rel="noreferrer">
-                <i className="uil uil-globe"></i>
-              </a>
-              <a href={`${GitHub}`} className="projects_social-icon" target="_blank" rel="noreferrer">
-                <i className="uil uil-github-alt"></i>
-              </a>
-            </div>
-          </SwiperSlide>
-        ))}
+        {firstRowProjects.map(
+          ({ id, title, description, image, GitHub, hosted }) => (
+            <SwiperSlide className='projects__card' key={id}>
+              <h3 className='projects__name'>{title}</h3>
+              <img src={image} alt='img' className='projects__img' />
+              <div className='projects__description'>{description}</div>
+              <div>
+                <a
+                  href={`mailto:mazine+alumni.brown.edu?subject=request to schedule demo- ${title}`}
+                  className='projects_social-icon'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <i className='uil uil-globe'></i>
+                </a>
+                <a
+                  href={`mailto:mazine+alumni.brown.edu?subject=request to view project- ${title}`}
+                  className='projects_social-icon'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <i className='uil uil-github-alt'></i>
+                </a>
+              </div>
+            </SwiperSlide>
+          ),
+        )}
       </Swiper>
 
       {/* Second Swiper instance for the second row */}
       <Swiper
-        className="projects__container"
+        className='projects__container projects__container--fullwidth'
         loop={true}
-        grabCursor={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        allowTouchMove={false}
+        freeMode={{
+          enabled: true,
+          momentum: false,
+        }}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: false,
+        }}
+        speed={8000}
         slidesPerView={3}
         spaceBetween={24}
-        // pagination={{ clickable: true }}
-        modules={[Pagination, Autoplay]}
+        modules={[Pagination, Autoplay, FreeMode]}
       >
-        {secondRowProjects.map(({ id, title, description, image, GitHub, hosted }) => (
-          <SwiperSlide className="projects__card" key={id}>
-            <h3 className="projects__name">{title}</h3>
-            <img src={image} alt="img" className="projects__img" />
-            <div className="projects__description">{description}</div>
-            <div>
-              <a href={`${hosted}`} className="projects_social-icon" target="_blank" rel="noreferrer">
-                <i className="uil uil-globe"></i>
-              </a>
-              <a href={`${GitHub}`} className="projects_social-icon" target="_blank" rel="noreferrer">
-                <i className="uil uil-github-alt"></i>
-              </a>
-            </div>
-          </SwiperSlide>
-        ))}
+        {secondRowProjects.map(
+          ({ id, title, description, image, GitHub, hosted }) => (
+            <SwiperSlide className='projects__card' key={id}>
+              <h3 className='projects__name'>{title}</h3>
+              <img src={image} alt='img' className='projects__img' />
+              <div className='projects__description'>{description}</div>
+              <div>
+                <a
+                  href={`mailto:mazine+alumni.brown.edu?subject=request to schedule demo- ${title}`}
+                  className='projects_social-icon'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <i className='uil uil-globe'></i>
+                </a>
+                <a
+                  href={`mailto:mazine+alumni.brown.edu?subject=request to view project- ${title}`}
+                  className='projects_social-icon'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <i className='uil uil-github-alt'></i>
+                </a>
+              </div>
+            </SwiperSlide>
+          ),
+        )}
       </Swiper>
     </section>
   );
